6ab958fc8b84777c4bf8ad59d31095cebf50367e
Daniel Wlazło <wlazlo.daniel@gmail.com>
2015-11-14 07:32:04 +0100

    Everything done in one file

--- Lab_1/Lab_1/Lab_1/Lab1.cpp
+++ Lab_1/Lab_1/Lab_1/Lab1.cpp
@@ -8,39 +8,59 @@ class Wektor2D
 {
 	friend Wektor2D operator + (const Wektor2D &w1, const Wektor2D &w2);
 public:
-	Wektor2D() : x(0.), y(0.) // Konstruktor domyœlny
+
+	//Konstruktory i destruktory
+
+	Wektor2D() : x(0.), y(0.) // Konstruktor domyœlny z list¹ inicjalizacyjn¹
 	{
-		//cout << "Domyslny ";
+		cout << "Domyslny ";
 		//Drukuj();
+		nr = indx++;
+		DrukujNumer();
 	} 
 	Wektor2D(const double &a, const double &b)  // Konstruktor parametryczny
 	{
 		x = a;
 		y = b;
-		//cout << "Parametryczny ";
+		cout << "Parametryczny ";
 		//Drukuj();
+		nr = indx++;
+		DrukujNumer();
 	}
 	Wektor2D(const Wektor2D &a) // Konstruktor kopiuj¹cy
 	{
 		x = a.x;
 		y = a.y;
-		//cout << "Kopiujacy ";
+		cout << "Kopiujacy ";
 		//Drukuj();
+		nr = indx++;
+		DrukujNumer();
 	}
-	~Wektor2D()
+	~Wektor2D()  // Destruktor
 	{
-		//cout << "Destruktor ";
+		cout << "Destruktor ";
 		//Drukuj();
+		DrukujNumer();
 	}
+	
+	//Funkcje drukuj¹ce
+
 	void Drukuj()
 	{
-		cout << setprecision(6) << "[ " << x << ", " << y << " ]" << endl;
+		cout << setprecision(6) << "[ " << x << ", " << y << " ]" << endl;  // setprecision odpowiada za d³ugoœæ znaków
 	}
+	void DrukujNumer() //Metoda do drukowania numeru :D 
+	{
+		cout << "Obiekt nr " << nr << endl;
+	}
+
+	//Operatory jednoargumentowe
+
 	const Wektor2D& operator += (const Wektor2D &w)
 	{
 		x += w.x;
 		y += w.y;
-		return *this;
+		return *this;  //zwróæ ten sam obiekt
 	}
 	const Wektor2D& operator += (const int &a)
 	{
@@ -48,6 +68,10 @@ public:
 		y += a;
 		return *this;
 	}
+	const Wektor2D& operator = (const Wektor2D &w){ return *this; }
+
+	//Akcesory
+
 	const double& GetX() const { return x; }
 	const double& GetY() const { return y; }
 
@@ -57,6 +81,7 @@ public:
 		{
 			x = MAX_VAL;
 			cout << "Przeroczono wartosc MAX_VAL" << endl;
+			return x;
 		}
 		x = a;
 		return x;
@@ -67,15 +92,23 @@ public:
 		{
 			y = MAX_VAL;
 			cout << "Przeroczono wartosc MAX_VAL" << endl;
+			return y;
 		}
 		y = a;
 		return y;
 	}
 
+
 private:
 	double x, y;
+	static int indx;  //zmienna statyczna - fajna rzecz
+	int nr;
 };
 
+//Inicjalizacja zmiennej statycznej
+int Wektor2D::indx = 0;
+
+//Operatory dwuargumentowe
 Wektor2D operator + (const Wektor2D &w1, const Wektor2D &w2)
 {
 	Wektor2D w;
@@ -85,22 +118,23 @@ Wektor2D operator + (const Wektor2D &w1, const Wektor2D &w2)
 }
 
 
+
 void main(void)
-{
-	cout << "Hello!" << endl;
+{	
 	Wektor2D v1(10, 20);
 	Wektor2D v2 = v1;
 	Wektor2D v3(v1);
 	Wektor2D v4;
-	Wektor2D v5;
-	v5 += 10;
-	Wektor2D v6 = v1 + v2;
-	v5.Drukuj();
-	v6.Drukuj();
-	v3.SetX(50);
+	v1.Drukuj();
+	v2.Drukuj();
 	v3.Drukuj();
-	//v1.Drukuj();
-	//v2.Drukuj();
-	//v3.Drukuj();
-	//v4.Drukuj();
+	v4.Drukuj();
+	v4 = v1 + v2;
+	v4.Drukuj();
+	v3 += v3;
+	v3.Drukuj();
+	v4.SetX(50);
+	v4.Drukuj();
+	Wektor2D v5(v4.GetY(), v3.GetX());
+	v5.Drukuj();
 }
